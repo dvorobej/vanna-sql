8bdb0a4e501f03d20f9451253418f8521fd63840
dvorobej <yagamilit@gmail.com>
2026-06-04 00:30:19 +0300

    clean up

--- src/config.py
+++ src/config.py
@@ -1,15 +1,12 @@
+import os
 from pathlib import Path
-
 from dotenv import load_dotenv
-from loguru import logger
 
 # Load environment variables from .env file if it exists
 load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
-
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
@@ -21,12 +18,7 @@ MODELS_DIR = PROJ_ROOT / "models"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
-# If tqdm is installed, configure loguru with tqdm.write
-# https://github.com/Delgan/loguru/issues/135
-try:
-    from tqdm import tqdm
-
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
+DENSE_EMBEDDING_MODEL = os.path.join(MODELS_DIR, "e5_large")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_URL = os.getenv("OPENAI_API_URL", "https://openrouter.ai/api/v1")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "openai/gpt-5.5")
